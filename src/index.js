a6f99e5408395f34d14b10dc160eb9e111c37a8a
Anu Lama <anulama@u.boisestate.edu>
2022-01-17 11:53:01 -0700

    added basename to Router component in index.js, added base element in index.html

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import store from './app/store';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <Router>
+  <Router basename='/cryptoapp'>
     <Provider store={store}>
       <App />
     </Provider>
